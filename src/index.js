cead82df52892ce5717224ec259b79a235b8ae91
louis-marceron <louis.marceron@protonmail.com>
2023-05-29 14:44:39 +0200

    feat: add express router reduce coupling with Netifly

---
+++ src/index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const path = require('path');
+
+const PORT = process.env.PORT || 5000;
+const app = express();
+const publicPath = path.join(__dirname, '/'); // serves from the root folder
+
+app.use(express.static(publicPath));
+
+app.get('*', (req, res) => {
+    console.log('test');
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
