8448bb123967b4b0e2b7bc267a2c59a615efb88a
louis-marceron <louis.marceron@protonmail.com>
2023-05-30 09:00:40 +0200

    flip effect

--- src/components/mealsList.js
+++ src/components/mealsList.js
@@ -12,12 +12,21 @@ export default {
         // Assign click handlers to all meal divs after the DOM has loaded
         const mealElements = document.querySelectorAll('.meal');
         mealElements.forEach(mealElement => {
-            mealElement.addEventListener('click', async () => {
+            mealElement.addEventListener('click', () => {
+                mealElement.classList.toggle('flipped');
+            });
+
+            // When clicking the confirm button, consume the meal and prevent the card from flipping back
+            const confirmButton = mealElement.querySelector('.confirm-button');
+            confirmButton.addEventListener('click', async (e) => {
+                e.stopPropagation(); // prevent the click from bubbling up to the .meal element
                 const mealId = mealElement.getAttribute('data-meal-id');
                 const response = await consumeMeal(mealId, getCookie('userId'));
                 if (response.error) {
                     alert(response.error);
-                } 
+                } else {
+                    mealElement.classList.remove('flipped'); // flip the card back
+                }
             });
         });
     }
@@ -26,11 +35,21 @@ export default {
 function createMeal(meal) {
     return `
     <div class="meal" data-meal-id="${meal.meal_id}">
-        <div class="texts">
-            <h2>${meal.name}</h2>
-            <p>${meal.description}</p>
+        <div class="meal-front">
+            <div class="texts">
+                <h2>${meal.name}</h2>
+                <p>${meal.description}</p>
+            </div>
+            <img class="food-icon" src="${meal.image_url}" alt="${meal.name} logo" />
         </div>
-        <img src="${meal.image_url}" alt="${meal.name} logo" />
+        <div class="meal-back">
+            <div class="confirm-message">
+                <div class="refuse-button">
+                    <img src="https://cdn.discordapp.com/attachments/940022815439085620/1112992966475259994/back.svg" class="back-arrow" alt="back arrow"/>
+                </div>
+                <button class="confirm-button">Consommer ce repas</button>
+            </div>
+        </div>
     </div>
   `
 }
